1ff133c2a36a7de8d25e5247217d39d7527cb268
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-10-25 13:53:43 +0900

    Creating an array with product details

---
+++ src/data/productDetails.js
@@ -0,0 +1,22 @@
+const productDetails = [
+    {
+        id: "1",
+        title: "Fall Limited Edition Sneakers",
+        src: [
+            "/public/img/image-product-1.jpg",
+            "/public/img/image-product-1-thumbnail.jpg",
+            "/public/img/image-product-2.jpg",
+            "/public/img/image-product-2-thumbnail.jpg",
+            "/public/img/image-product-3.jpg",
+            "/public/img/image-product-3-thumbnail.jpg",
+            "/public/img/image-product-4.jpg",
+            "/public/img/image-product-4-thumbnail.jpg",
+        ],
+        description:
+            "These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.",
+        price: 125,
+        count: 1,
+    },
+];
+
+export default productDetails;
