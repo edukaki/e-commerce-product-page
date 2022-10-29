ae717f017f5dc2b3addf5dfd4b3fd4e6d47a6883
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-10-30 00:27:51 +0900

    Creating function that checks if the new item already exists in the cart

--- src/components/BtnCart/index.js
+++ src/components/BtnCart/index.js
@@ -1,19 +1,36 @@
 import React, { useState } from 'react'
 import "./BtnCart.css"
 import cartIcon from "../../svg/icon-cart.svg"
+import trash from "../../svg/icon-delete.svg"
 
-const BtnCart = (props) => {
-    const data = !props.arr ? "Your cart is empty" : props.arr;
+const BtnCart = ({ cartItems }) => {
     const [showCart, setShowCart] = useState(false);
 
     return (
         <div className="nav-cart">
             <img src={cartIcon} alt='cart' width="20" onClick={() => setShowCart(!showCart)} />
-            <span>0</span>
+            <span className='nav-cart--counter'>0</span>
             {showCart && (
                 <div className='btn__cart--show'>
                     <h3 className='cart__title'>Cart</h3>
-                    {data}
+                    {cartItems.length === 0 && (
+                        <div>Your cart is empty.</div>
+                    )}
+                    {cartItems.map((item) => (
+                        <div key={item.id} className="cart__items__list">
+                            <div className='cart__items__list--container'>
+                                <img className='cart__items__list--img' src={item.src[0]} alt="Products" />
+                                <div>
+                                    <p>{item.title}</p>
+                                    <div className='price__container'>
+                                        <p>${item.price}.00 x {item.quantity} <strong>&nbsp;${item.price * item.quantity}</strong></p>
+                                    </div>
+                                </div>
+                                <button className='cart__items__list--trash'><img src={trash} alt="Trash icon" /></button>
+                            </div>
+
+                        </div>
+                    ))}
                 </div>
             )}
         </div>
