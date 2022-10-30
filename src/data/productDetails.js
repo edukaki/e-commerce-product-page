e01ac32fd0ca3edae29eb07f05727ad98b1a014a
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-10-30 17:14:28 +0900

    Importing product data

--- src/data/productDetails.js
+++ src/data/productDetails.js
@@ -1,19 +1,236 @@
-const productDetails = [
-    {
-        id: "1",
-        title: "Fall Limited Edition Sneakers",
-        src: [
-            "https://assets.atmos-tokyo.com/items/L/dz5352-847-1.jpg",
-            "https://assets.atmos-tokyo.com/items/1/dz5352-847-2.jpg",
-            "https://assets.atmos-tokyo.com/items/2/dz5352-847-3.jpg",
-            "https://assets.atmos-tokyo.com/items/3/dz5352-847-4.jpg"
+const productDetails = {
+    productItems: [
+        {
+            id: 1,
+            collection: "women",
+            title: "NIKE W AIR MAX 1 PRM STARFISH",
+            src: [
+                "https://assets.atmos-tokyo.com/items/L/dz5352-847-1.jpg",
+                "https://assets.atmos-tokyo.com/items/1/dz5352-847-2.jpg",
+                "https://assets.atmos-tokyo.com/items/2/dz5352-847-3.jpg",
+                "https://assets.atmos-tokyo.com/items/3/dz5352-847-4.jpg"
 
-        ],
-        description:
-            "These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.",
-        price: 125,
-        count: 1,
-    },
-];
+            ],
+            description:
+                "Celebrate with us in the Air Max 1. Lemon wash and bright accents of mantra orange playfully portray Mexican marigolds, also known as cempascitol. Widely used in Mexico's Day of the Dead festival, the flower is believed to be a sign that guides the souls of the deceased into the world. Speaking of marigolds, also note the debossed floral graphics around the mudguard. It adds a lively touch and pays further homage to Mexican traditions. Don't worry when the sun goes down and it gets dark. A glow-in-the-dark outsole completes the outfit with a candlelit look. In our memories and in our hearts, we will always be \"Somos Familia\" (we are family).",
+            price: 125,
+            count: 1,
+        },
+        {
+            id: 2,
+            collection: "men",
+            title: "NIKE LUNAR FORCE 1 DUCKBOOT",
+            src: [
+                "https://assets.atmos-tokyo.com/items/L/dz5320-200-1.jpg",
+                "https://assets.atmos-tokyo.com/items/L/dz5320-200-2.jpg",
+                "https://assets.atmos-tokyo.com/items/L/dz5320-200-3.jpg",
+                "https://assets.atmos-tokyo.com/items/L/dz5320-200-4.jpg",
+            ],
+            description:
+                "Even if it's colder and wetter than the other side of the moon, you still need stylish style. The Luna Force 1 features premium water-repellent leather and a durable outsole design. Arrange the AF1 style to keep the flames burning even in bad weather. The star-studded design on the toe creates an edgy style.",
+            price: 185,
+            count: 1,
+        },        {
+            id: 3,
+            collection: "men",
+            title: "NIKE AIR FORCE 1 '07 PRM",
+            src: [
+                "https://assets.atmos-tokyo.com/items/L/dx2305-100-1.jpg",
+                "https://assets.atmos-tokyo.com/items/L/dx2305-100-2.jpg",
+                "https://assets.atmos-tokyo.com/items/L/dx2305-100-3.jpg",
+                "https://assets.atmos-tokyo.com/items/L/dx2305-100-4.jpg",
+            ],
+            description:
+                "A shoe that has been called a legend in two fields. This Air Force 1 celebrates the origins of hip-hop and the sneaker icon's 40th anniversary in the Bronx. Since its birth in 1982, the Air Force 1 has been deeply associated with the world's most influential music genre, hip-hop, and has become a uniform presence in that culture. The dubre is stamped with two important dates: 1973 and 1982. It highlights the now widely known beginning year of hip-hop and Air Force 1. In addition, the Swoosh and wide shoelaces were used to create a sense of unity by scratching the record, and the thickness of the shoelaces determined \"coolness\".",
+            price: 140,
+            count: 1,
+        },        {
+            id: 4,
+            collection: "men",
+            title: "NIKE ZOOM LEBRON II",
+            src: [
+                "https://assets.atmos-tokyo.com/items/L/dr0826-100-1.jpg",
+                "https://assets.atmos-tokyo.com/items/L/dr0826-100-2.jpg",
+                "https://assets.atmos-tokyo.com/items/L/dr0826-100-3.jpg",
+                "https://assets.atmos-tokyo.com/items/L/dr0826-100-4.jpg",
+            ],
+            description:
+                "Celebrate King's first-ever signature shoe in red, white and blue while complementing the game with a design that's faithful to the original version. Iconic American colors capture a special historic moment when a young LeBron James showcased an unknown talent. The graphics on the collar and tongue inherit the details you'd expect from this shoe. Laser-engraved graphics, ballistic material on the sides, and taut leather complete the look that shines on the world stage.",
+            price: 230,
+            count: 1,
+        },        {
+            id: 5,
+            collection: "men",
+            title: "THE NETWORK BUSINESS ADDICT HOODIE NAVY 22FA-I",
+            src: [
+                "https://assets.atmos-tokyo.com/items/L/tnbc0141-0157-1.jpg",
+                "https://assets.atmos-tokyo.com/items/L/tnbc0141-0157-2.jpg",
+                "https://assets.atmos-tokyo.com/items/L/tnbc0141-0157-3.jpg",
+                "https://assets.atmos-tokyo.com/items/L/tnbc0141-0157-4.jpg",
+            ],
+            description:
+                "A hoodie hooked up to AJ4's \"Midnight Navy\". Using a 12.4 ounce heavyweight fabric body, the front is \"SNEAKER Addict\", which means sneaker addicts, and the back is \"WE'VE ONLY TWO FEET\" with a strong presence. The TNB original logo is printed on both sleeves, and a TNB silicon patch is sewn on the back hem.",
+            price: 130,
+            count: 1,
+        },        {
+            id: 6,
+            collection: "men",
+            title: "TOKYO 23 RENEGADES QUARTER-ZIP CADET COLLAR SWEAT ASH 22FW-I",
+            src: [
+                "https://assets.atmos-tokyo.com/items/L/t23-22-037-ash-1.jpg",
+                "https://assets.atmos-tokyo.com/items/L/t23-22-037-ash-2.jpg",
+                "https://assets.atmos-tokyo.com/items/L/t23-22-037-ash-3.jpg",
+                "https://assets.atmos-tokyo.com/items/L/t23-22-037-ash-4.jpg",
+                
+
+            ],
+            description:
+                "A half-zip stand-up collar sweatshirt with the motif of <RENEGADES>, a fictitious basketball team established to have a strong impact on the Japanese basketball scene. 15 oz super heavyweight sweatshirt. The ONEITA POWER-SWEAT is made with 100% cotton fleece that has been washed and has a moderately loose fit. Comfortable even with heavyweight material. Sweat pants (t23-22-036) and setup style are also possible.",
+            price: 200,
+            count: 1,
+        },        {
+            id: 7,
+            collection: "men",
+            title: "PUMA SUEDE VTG DIASPORA ATMOS BIEGE 22FA-S",
+            src: [
+                "https://assets.atmos-tokyo.com/items/L/389167-01-1.jpg",
+                "https://assets.atmos-tokyo.com/items/L/389167-01-2.jpg",
+                "https://assets.atmos-tokyo.com/items/L/389167-01-3.jpg",
+                "https://assets.atmos-tokyo.com/items/L/389167-01-4.jpg",
+            ],
+            description:
+                "SUEDE VTG of triple collaboration with Diaspora skateboards, PUMA, and atmos appears. The light blue color scheme, which is the key color of Diaspora skateboards, is accented, and the stitching on the upper, which is rarely seen in PUMA SUEDE, is a major feature. A premium embossed leather formstrip with PUMA and Magic Ciecle logos on the tongue. In addition, the Fraternity Logo is printed on the shoelace tip. The fat shoelace that symbolizes PUMA SUEDE is also included, making it a pair of shoes full of collaboration.",
+            price: 120,
+            count: 1,
+        },        {
+            id: 8,
+            collection: "men",
+            title: "adidas ADIMATIC GRAY ONE",
+            src: [
+                "https://assets.atmos-tokyo.com/items/L/gy2091-1.jpg",
+                "https://assets.atmos-tokyo.com/items/L/gy2091-2.jpg",
+                "https://assets.atmos-tokyo.com/items/L/gy2091-3.jpg",
+                "https://assets.atmos-tokyo.com/items/L/gy2091-4.jpg",
+            ],
+            description:
+                "New colors for the holiday season are now available from ADIMATIC, which has been reprinted this year. The gray upper and black 3-stripes coloring make it a pair that can be used regardless of styling.",
+            price: 120,
+            count: 1,
+        },        {
+            id: 9,
+            collection: "women",
+            title: "FILA DISRUPTOR II EXP x atmos pink Pink",
+            src: [
+                "https://assets.atmos-tokyo.com/items/L/wfw22066-661-1.jpg",
+                "https://assets.atmos-tokyo.com/items/L/wfw22066-661-2.jpg",
+                "https://assets.atmos-tokyo.com/items/L/wfw22066-661-3.jpg",
+                "https://assets.atmos-tokyo.com/items/L/wfw22066-661-4.jpg",
+            ],
+            description:
+                "A collaboration sneaker with the standard model \"DISRUPTOR Ⅱ EXP (Disruptor 2 EXP)\", which is synonymous with atmos pink and FILA sneakers! A thick and lightweight platform design with a uniform upper color design. The atmos pink logo is used on the tongue for a cute finish. In addition to being more comfortable than it looks, the style-up effect will also raise your mood. Add color to a standard item that matches various styles.",
+            price: 99,
+            count: 1,
+        },       {
+            id: 10,
+            collection: "women",
+            title: "PUMA MAYZE WEDGE WNS CORDUROY ATMOS LIGHT",
+            src: [
+                "https://assets.atmos-tokyo.com/items/L/391915-01-1.jpg",
+                "https://assets.atmos-tokyo.com/items/L/391915-01-2.jpg",
+                "https://assets.atmos-tokyo.com/items/L/391915-01-3.jpg",
+                "https://assets.atmos-tokyo.com/items/L/391915-01-4.jpg",
+            ],
+            description:
+                "Atmos pink x PUMA collaboration sneakers are here! Corduroy is used for the upper, and boa is used for the inner, making it a warm pair even when worn in the cold season♡ The tongue uses the PUMA logo and atmos pink heart logo for a cute finish! The silhouette of the wedge sole that slopes down to the heel part will enhance your style.",
+            price: 85,
+            count: 1,
+        },       {
+            id: 11,
+            collection: "women",
+            title: "atmos pink ムートンショルダーバッグ BROWN 22FA-I",
+            src: [
+                "https://assets.atmos-tokyo.com/items/L/22aw-bg04-brn-1.jpg",
+                "https://assets.atmos-tokyo.com/items/L/22aw-bg04-brn-2.jpg",
+                "https://assets.atmos-tokyo.com/items/L/22aw-bg04-brn-3.jpg",
+                "https://assets.atmos-tokyo.com/items/L/22aw-bg04-brn-4.jpg",
+
+            ],
+            description:
+                "Mouton bag using fake suede and boa perfect for winter. Outstanding functionality with pockets on the outside and inside. It is a bag with a design that will accent your outfit.",
+            price: 85,
+            count: 1,
+        },       {
+            id: 12,
+            collection: "women",
+            title: "atmos pink キルティング スポーティーバック BLACK 22FA-I",
+            src: [
+                "https://assets.atmos-tokyo.com/items/L/22aw-bg02-blk-1.jpg",
+                "https://assets.atmos-tokyo.com/items/L/22aw-bg02-blk-2.jpg",
+                "https://assets.atmos-tokyo.com/items/L/22aw-bg02-blk-3.jpg",
+                "https://assets.atmos-tokyo.com/items/L/22aw-bg02-blk-4.jpg",
+            ],
+            description:
+                "A cute bag with a padded silhouette. We are particular about the catchy color scheme of the lining♡ Because the opening is wide and the contents are easy to take out, functionality is also ◎",
+            price: 80,
+            count: 1,
+        },       {
+            id: 13,
+            collection: "women",
+            title: "atmos pink カーゴ風 デニムオールインワン BLUE 22FA-I",
+            src: [
+                "https://assets.atmos-tokyo.com/items/L/22aw-ayop01-blu-1.jpg",
+                "https://assets.atmos-tokyo.com/items/L/22aw-ayop01-blu-2.jpg",
+                "https://assets.atmos-tokyo.com/items/L/22aw-ayop01-blu-3.jpg",
+                "https://assets.atmos-tokyo.com/items/L/22aw-ayop01-blu-4.jpg",
+            ],
+            description:
+                "A denim all-in-one with a cargo-style design. We are particular about the silhouette and details so that you can see the setup. It also features a color that has a vintage feel due to the touch processing. A stylish and easy item that can be coordinated with just one piece!",
+            price: 150,
+            count: 1,
+        },       {
+            id: 14,
+            collection: "women",
+            title: "atmos pink 異素材 デニムカーゴパンツ BLUE 22FA-I",
+            src: [
+                "https://assets.atmos-tokyo.com/items/L/22aw-aypt08-blu-1.jpg",
+                "https://assets.atmos-tokyo.com/items/L/22aw-aypt08-blu-2.jpg",
+                "https://assets.atmos-tokyo.com/items/L/22aw-aypt08-blu-3.jpg",
+                "https://assets.atmos-tokyo.com/items/L/22aw-aypt08-blu-4.jpg",
+            ],
+            description:
+                "Denim cargo pants with different material pockets. It has a balloon-like silhouette that tapers loosely toward the hem. It can also be worn in a low-rise style, making it a perfect item for trendy styles.",
+            price: 125,
+            count: 1,
+        },       {
+            id: 15,
+            collection: "women",
+            title: "atmos pink × RIEHATA グラフィックビッグT BLACK 22FA-I",
+            src: [
+                "https://assets.atmos-tokyo.com/items/L/22aw-retp02-blk-1.jpg",
+                "https://assets.atmos-tokyo.com/items/L/22aw-retp02-blk-2.jpg",
+                "https://assets.atmos-tokyo.com/items/L/22aw-retp02-blk-3.jpg",
+                "https://assets.atmos-tokyo.com/items/L/22aw-retp02-blk-4.jpg",
+            ],
+            description:
+                "atmos pink × RIEHATA collaboration item A big T with boldly printed portrait of Mr. RIEHATA drawn by artist NANCY SNAKE. It can be worn by both men and women as it is unisex. A must-see item for RIEHATA fans, a big T-shirt unique to collaboration with a single piece!",
+            price: 55,
+            count: 1,
+        },       {
+            id: 16,
+            collection: "women",
+            title: "atmos pink × RIEHATA ANGEL ショートZIPフーディ",
+            src: [
+                "https://assets.atmos-tokyo.com/items/L/22aw-retp03-wht-1.jpg",
+                "https://assets.atmos-tokyo.com/items/L/22aw-retp03-wht-2.jpg",
+                "https://assets.atmos-tokyo.com/items/L/22aw-retp03-wht-3.jpg",
+                "https://assets.atmos-tokyo.com/items/L/22aw-retp03-wht-4.jpg",
+            ],
+            description:
+                "atmos pink × RIEHATA collaboration item Short zip hoodie in sweatshirt material. The double zipper design allows you to freely show your skin for a trendy look. The spray art style ANGEL graphic printed on the hood is drawn by artist NANCY SNAKE.",
+            price: 120,
+            count: 1,
+        },        
+    ]
+}
 
 export default productDetails;
